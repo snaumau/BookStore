97900a02b65e0926787a6f7af68925696af1bdc3
Siarhei Naumau <s.naumau@gmail.com>
2023-08-05 14:34:02 +0300

    feat: add order configurations

---
+++ Infrastructure/Data/Config/OrderConfiguration.cs
@@ -0,0 +1,15 @@
+using Core.Entities.Order;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasMany(o => o.OrderItems)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Infrastructure/Data/Config/OrderItemConfiguration.cs
@@ -0,0 +1,16 @@
+using Core.Entities.Order;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.OwnsOne(i => i.BookItem,
+            oi => {oi.WithOwner();});
+        builder.Property(i => i.Price)
+            .HasColumnType("decimal(18,2)");
+    }
+}
