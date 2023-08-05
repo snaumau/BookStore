80046220dc956ed39abf265adcb0dbde42483c0e
Siarhei Naumau <s.naumau@gmail.com>
2023-08-05 23:12:18 +0300

    feat: add required for properties in the models

--- API/DTO/BookDto.cs
+++ API/DTO/BookDto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTO;
 
 public record BookDto
 {
     public int Id { get; set; }
+    
+    [Required]
     public string Name { get; set; } = string.Empty;
+    
+    [Required]
     public decimal Price { get; set; }
+    
     public DateTime ReleaseDate { get; set; }
 }
--- Core/Entities/Book.cs
+++ Core/Entities/Book.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Core.Entities;
 
 public class Book : BaseEntity
 {
+    [Required]
     public string Name { get; set; } = string.Empty;
 
+    [Required]
     public decimal Price { get; set; }
 
-    public DateTime ReleaseDate { get; set; }
+    public DateTime ReleaseDate { get; set; } = DateTime.Now;
 }
