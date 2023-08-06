83a0967409bdff10945c51726249e580e7143e9e
Siarhei Naumau <s.naumau@gmail.com>
2023-08-06 10:24:19 +0300

    refactor: data annotation

--- API/DTO/BookDto.cs
+++ API/DTO/BookDto.cs
@@ -7,9 +7,11 @@ public record BookDto
     public int Id { get; set; }
     
     [Required]
+    [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
     
     [Required]
+    [Range(0.1, double.MaxValue, ErrorMessage = "Price must be greater than zero")]
     public decimal Price { get; set; }
     
     public DateTime ReleaseDate { get; set; }
